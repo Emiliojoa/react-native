f7aa364aad426c34b9eea6edd329ca8d9b5f041c
Emilio Joaquin <167462234+Emiliojoa@users.noreply.github.com>
2025-05-10 22:05:06 -0300

    firt commit

--- App.js
+++ App.js
@@ -4,7 +4,8 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Welcome a mi app desde react Native</Text>
+      <Text>Hola desde mi celu</Text>
       <StatusBar style="auto" />
     </View>
   );
